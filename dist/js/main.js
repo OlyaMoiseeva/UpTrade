2449384e4063b5df9f1c54e0d3241130e05a1ae6
OlyaMoiseeva <moiseevaolya666@yandex.ru>
2020-02-21 03:39:26 +0300

    upload

---
+++ dist/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(() => {
+
+  const tab = '.tabs-box__link';
+  const accordion = '.tabs__accordion';
+  const active = 'tabs__active-arr';
+
+  const showTabs = () => {
+    if (window.matchMedia('(min-width: 481px)').matches) {
+      $('.tabs__tab-content:first').show();
+      $('.list__but').val('Продолжить');
+    } else {
+      $('.tabs__tab-content').hide();
+      $(accordion).removeClass(active);
+      $('.tabs').removeClass('tabs_bg');
+      $('.choose').removeClass('choose_bg');
+      $('.list__but').val('Открыть отчет');
+    }
+  }
+
+  showTabs();
+
+  $(window).on('resize', () => {
+    showTabs();
+  });
+//tabs
+  $(`${tab}, .choose__circle`).on('click', function () {
+    const idx = $(this).index();
+    $('.tabs__tab-content').hide();
+    const activeTab = $(this).attr('rel');
+    $(`#${activeTab}`).fadeIn();
+    $(tab)
+      .removeClass('tabs-box__link_active')
+      .eq(idx)
+      .addClass('tabs-box__link_active');
+    $(accordion).removeClass(active);
+    $(`${accordion}[rel^='${activeTab}']`).addClass(active);
+  });
+//accordion
+  $(accordion).on('click', function () {
+    const activeTab = $(this).attr('rel');
+    $('.tabs__tab-content').not('#' + activeTab).hide();
+    $(`#${activeTab}`).fadeToggle(0);
+
+    if ($(`#${activeTab}`).is(':visible')) {
+      $('.tabs').addClass('tabs_bg');
+      $('.choose').addClass('choose_bg');
+    } else {
+      $('.tabs').removeClass('tabs_bg');
+      $('.choose').removeClass('choose_bg');
+    }
+
+    $(accordion).not(this).removeClass(active);
+    $(this).toggleClass(active);
+    $(tab).removeClass('tabs-box__link_active');
+    $(`${tab}[rel^='${activeTab}']`).addClass('tabs-box__link_active');
+  });
+});
